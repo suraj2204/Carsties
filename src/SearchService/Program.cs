ef6b102355ce1001b1fc464d21b70bed613448db
suraj2204 <surajspardhi@gmail.com>
2026-04-04 23:21:48 +0530

    Adding New Classlib

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -13,6 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+       cfg.ConfigureEndpoints(context); 
+    });
+});
 
 var app = builder.Build();
 
