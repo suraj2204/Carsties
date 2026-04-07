083591ac9a34c8bb2a9477ceb2b89e5c7afb0b63
suraj2204 <surajspardhi@gmail.com>
2026-04-07 22:43:21 +0530

    Using Message retries

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,12 +16,14 @@ namespace AuctionService.Controllers
     {
         private readonly AuctionDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IPublishEndpoint _publishEndpoint;
 
         public AuctionsController(AuctionDbContext context,
-                                IMapper mapper)
+                                IMapper mapper, IPublishEndpoint publishEndpoint)
         {
             _context = context;
             _mapper = mapper;
+            _publishEndpoint = publishEndpoint;
         }
 
         [HttpGet]
@@ -51,13 +55,20 @@ namespace AuctionService.Controllers
         public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
         {
             var auction = _mapper.Map<Auction>(auctionDto);
+
             auction.Seller = "Test";
+
             _context.Auctions.Add(auction);
+
+             var newAuction = _mapper.Map<AuctionDto>(auction);
+
+            await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
             var result = await _context.SaveChangesAsync() > 0;
 
             if(!result) return BadRequest("Could not save result to DB");
 
-            return CreatedAtAction(nameof(GetAuctionById),new {auction.Id},_mapper.Map<AuctionDto>(auction));
+            return CreatedAtAction(nameof(GetAuctionById),new {auction.Id},newAuction);
         }
 
         [HttpPut("{id}")]
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -13,11 +14,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 builder.Services.AddMassTransit(x =>
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search",false));
+    
     x.UsingRabbitMq((context, cfg) =>
     {
+       cfg.ReceiveEndpoint("search-auction-created",e =>
+       {
+           e.UseMessageRetry(r => r.Interval(5,5));
+           e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+       });
        cfg.ConfigureEndpoints(context); 
     });
 });
